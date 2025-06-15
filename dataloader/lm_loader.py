9a87b944199804b2e9d8bd078116e1196239e983
Nishchal Chandel <nishchandel21@gmail.com>
2025-06-15 13:04:26 +0530

    Added DataLoader files to handle batch of data

---
+++ dataloader/lm_loader.py
@@ -0,0 +1,23 @@
+# Importing Libraries
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+# Defining Dataset Class
+class LMDataset(Dataset):
+    
+    def __init__(self, path):
+        self.data = torch.load(path)
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        return {
+            'input_ids' : self.data[idx]['input_ids'],
+            'attention_mask' : self.data[idx]['attention_mask']
+        }
+        
+# Defining DataLoader Function to load dataset
+def get_lm_dataloader(path, batch_size=4, shuffle=True, num_workers=2, pin_memory=True):
+    dataset = LMDataset(path)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=pin_memory)
